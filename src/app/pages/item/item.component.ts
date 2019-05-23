fcaad1bbfc0d5aafc49b74cb3a8a2da6073aef53
ignacioIbarra <i.ibarra.lopez@gmail.com>
2019-05-23 12:48:34 -0400

    fin sección 7

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductosService } from '../../services/productos.service';
-import { Producto } from '../../interfaces/producto.interface';
 import { ProductoDescripcion } from '../../interfaces/producto-descripcion.interface';
 
 @Component({
@@ -10,22 +9,27 @@ import { ProductoDescripcion } from '../../interfaces/producto-descripcion.inter
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
-producto: ProductoDescripcion;
-id: string;
+
+  producto: ProductoDescripcion;
+  id: string;
 
 
-  constructor(private route: ActivatedRoute,
-    public productoSevice: ProductosService) { }
+  constructor( private route: ActivatedRoute,
+               public productoService: ProductosService ) { }
 
-  ngOnInit( ) {
-    this.route.params.subscribe(parametros =>{
-      // console.log(parametros['id']);
-      this.productoSevice.getProductos(parametros['id'])
-      .subscribe((producto: ProductoDescripcion) =>{
-        this.id=parametros['id'];
-        this.producto= producto;
-      });
-    });
+  ngOnInit() {
+
+    this.route.params
+        .subscribe( parametros => {
+          // console.log(parametros['id']);
+          this.productoService.getProducto(parametros['id'])
+                .subscribe( (producto: ProductoDescripcion) => {
+                  this.id = parametros['id'];
+                  this.producto = producto;
+                });
+        });
+
+
   }
 
 }
